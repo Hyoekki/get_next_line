91ccf81e7f5b7ea599c490c83f71d9926883898d
Janne Hyokki <janne.hyokki@gmail.com>
2025-01-09 20:16:57 +0200

    change memcpy to memmove

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jhyokki <jhyokki@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 13:35:39 by jhyokki           #+#    #+#             */
-/*   Updated: 2024/12/26 15:02:46 by jhyokki          ###   ########.fr       */
+/*   Updated: 2025/01/09 20:16:10 by jhyokki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 char	*ft_strdup(const char *s1);
 
 #endif
